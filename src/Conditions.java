116d02e1eb45d0bc9cb32124de97a9fd602856e0
Kisten <33459026+KistenPM@users.noreply.github.com>
2024-02-01 16:20:06 +0700

    Second half lesson of Basics

--- src/Conditions.java
+++ src/Conditions.java
@@ -3,15 +3,38 @@ import java.util.Scanner;
 public class Conditions {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int a = 10;
 
-        int a;
-        a = sc.nextInt();
+        if (a == 10) { //условие
+            System.out.println("a = 10"); //то, что выполняется если условие верное
+        }
 
-        if (a == 10) {
-            System.out.println("a = 10");
-        } else {
-            System.out.println("a =/= 10");
+        Scanner intScanner = new Scanner(System.in);
+        Scanner stringScanner = new Scanner(System.in);
+
+        int b, c;
+        b = intScanner.nextInt();
+        String operator = stringScanner.nextLine();
+
+        c = intScanner.nextInt();
+
+        if (operator.equals("+")){
+            System.out.println(b + c);
+        }
+        else if (operator.equals("-")){
+            System.out.println(b - c);
+        }
+        else if (operator.equals("*")){
+            System.out.println(b * c);
+        }
+        else if (operator.equals("/")){
+            System.out.println(b / c);
+        }
+        else if (operator.equals("%")){
+            System.out.println(b % c);
+        }
+        else{
+            System.out.println("Вы ввели неверный знак вычисления");
         }
     }
 }
