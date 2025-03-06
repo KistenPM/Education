6b8a6d0690cb82b5394e13e1c92b86e7bfd34af3
Kisten <33459026+KistenPM@users.noreply.github.com>
2025-03-06 17:02:55 +0700

    Massive class test2

--- src/massive.java
+++ src/massive.java
@@ -3,5 +3,12 @@ public class massive {
     public static void main (String[] args) {
 
         System.out.println("He-he or not He-he");
+
+        var x=("divan");
+        var y=10;
+        System.out.println(x);
+        System.out.println(y);
+        final var LIMIT = 5;
+        System.out.println(LIMIT);
     }
 }
