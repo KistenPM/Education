9a1c0dbf6d96e873edd3fc945a0ea39c920b59bc
Kisten <33459026+KistenPM@users.noreply.github.com>
2025-03-25 02:06:52 +0700

    Pinetree

--- src/Paintree.java
+++ src/Paintree.java
@@ -12,14 +12,17 @@ public class Paintree {
 
         for (int p = 1; p < branches; p++) {
             System.out.println("-");
-//            for (int i = 1; i < branches; i++) {
-//                System.out.print("+");
-//            }
-            for (int n = 1; n <= p; n++) {
+            for (int i = 1; i < branches-p; i++) {
+                System.out.print(" ");
+            }
+            for (int n = 1; n < p*2; n++) {
                 System.out.print("*");
             }
         }
         System.out.println("-");
-        System.out.println("|");
+        for (int k = 1; k < branches/2; k++) {
+            System.out.print("  ");
+        }
+        System.out.print("|");
     }
 }
