317141c37f495a3fa32954f605d8e5752f5335ad
Kisten <33459026+KistenPM@users.noreply.github.com>
2025-03-29 18:47:09 +0700

    Pinetree final version

--- src/Paintree.java
+++ src/Paintree.java
@@ -4,25 +4,34 @@ public class Paintree {
     public static void main(String[] args) {
 
         System.out.println("Hello there, unknown user.");
-        System.out.print("Enter the number of branches:");
+        System.out.print("Enter the number of branches: ");
 
         Scanner in = new Scanner(System.in);
 
         int branches = in.nextInt();
 
-        for (int p = 1; p < branches; p++) {
+        for (int p = 2; p <= branches; p++) {
+            System.out.print(" ");
+        }
+        for (int i = 1; i < branches; i++) {
             System.out.println("-");
-            for (int i = 1; i < branches-p; i++) {
+            for (int n = 1; n < branches-i; n++) {
                 System.out.print(" ");
             }
-            for (int n = 1; n < p*2; n++) {
+            System.out.print("-");
+            for (int k = 1; k < i*2; k++) {
                 System.out.print("*");
             }
         }
         System.out.println("-");
-        for (int k = 1; k < branches/2; k++) {
+        for (int y = 1; y < branches/2; y++) {
             System.out.print("  ");
         }
-        System.out.print("|");
+        if (branches % 2 == 0) {
+            System.out.print(" ||");
+        }
+        else {
+            System.out.print("  ||");
+        }
     }
 }
