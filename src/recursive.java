7188b3b54b8cb6401f0f1ba1ddc879bef85efcec
Kisten <33459026+KistenPM@users.noreply.github.com>
2025-06-14 03:07:07 +0700

    Recursive example

--- src/recursive.java
+++ src/recursive.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class recursive {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        System.out.println(factorial(a));
+        int b = sc.nextInt();
+        System.out.println(fibonachi(b));
+    }
     static int factorial(int c){
-        int result = 1;
-        for (int i = 1; i <= c; i++) {
-            result *= i;
+        if (c == 0) {
+        return 0;
         }
-        return result;
+        if (c == 1) {
+            return 1;
+        }
+        return c * factorial(c - 1);
+    }
+    static int fibonachi(int n){
+
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        else{
+            return fibonachi(n - 1) + fibonachi(n - 2);
+        }
     }
 }
