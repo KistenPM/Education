308f5d9127e7db1468f83cc45c309d4d648ae152
Kisten <33459026+KistenPM@users.noreply.github.com>
2025-07-17 00:48:29 +0700

    Almost Fixed

--- src/KKLPerID.java
+++ src/KKLPerID.java
@@ -6,26 +6,23 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class KKLPerID {
-    Scanner sc = new Scanner(System.in);
-    public void main(String[] args){
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
         System.out.print("Enter Nickname:");
         String nick= sc.nextLine();
-        System.out.println();
         System.out.print("Enter ID:");
-        String id= sc.nextLine();
-        System.out.println();
+        int id = sc.nextInt();
 
         Period period = Period.of(3, 7, 11);
-        Duration durationD = Duration.from(period);
-        System.out.println(durationD+"вы с нами. ");
+        System.out.println(period+" вы с нами. ");
 
         LocalTime first = LocalTime.of(12, 15, 25);
         LocalTime last = LocalTime.of(15, 30, 50);
         Duration durationT = Duration.between(first, last);
 
         long hours = durationT.toHours();
-        long minutes = durationT.toMinutes();
-        long seconds = durationT.getSeconds();
+        long minutes = durationT.toMinutesPart();
+        long seconds = durationT.toSecondsPart();
 
         String Duration = String.format("%02d:%02d:%02d", hours, minutes, seconds);
         System.out.println("Время текущей сессии:"+Duration);
@@ -35,20 +32,19 @@ public class KKLPerID {
 
         KKL.Nickname=nick;
         KKL.ID=id;
-        KKL.strAge= String.valueOf(durationD);
+        KKL.strAge=period.toString();
         KKL.strTime=Duration;
         KKL.displayInfo();
-        System.out.println("meow");
     }
 }
 class Person {
     String Nickname;    //Никнейм пользователя
-    String ID;          //Уникальный ID пользователя
+    int ID;          //Уникальный ID пользователя
     String strAge;      //Сколько времени с первого захода пользователя
     LocalDate Date = LocalDate.parse(strAge,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     String strTime;     //Сколько времени у пользователя за текущую сессию
     LocalTime Time = LocalTime.parse(strTime,DateTimeFormatter.ofPattern("HH:mm:ss"));
     public void displayInfo(){
-        System.out.printf("Nickname: %s \tID: %s \tAge: %d \tTime: %s\n",Nickname,ID,strAge,strTime);
+        System.out.printf("Nickname: %s \tID: %s \tAge: %s \tTime: %s\n",Nickname,ID,strAge,strTime);
     }
 }
