bd6dc6cdfb00c9b6b6bb97358738c8eb194297d1
KistenPM <Mr.DjRepchik@mail.ru>
2025-08-07 01:21:26 +0700

    new file:   src/Heart.java

---
+++ src/Heart.java
@@ -0,0 +1,17 @@
+public class Heart {
+    public static void main(String[] args) {
+        String heart = 
+              "  **       **  \n"
+            + " *   * * *   * \n"
+            + "*      *      *\n"
+            + "*             *\n"
+            + " *           * \n"
+            + "  *         *  \n"
+            + "   *       *   \n"
+            + "    *     *    \n"
+            + "      * *      \n"
+            + "       *       \n";
+        
+        System.out.println(heart);
+    }
+}
